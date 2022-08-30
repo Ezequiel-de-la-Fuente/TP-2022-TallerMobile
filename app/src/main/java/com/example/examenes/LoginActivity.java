da22f9c451558d539a2ae373d6963af2479d6cf2
Ezequiel Ignacio de la Fuente <ezequieldelafuente2001@gmail.com>
2022-08-29 21:10:58 -0300

    TP-03: Cambie el diseño al login y le agregue la funcionalidad de cambiar la visibilidad de la contraseña.

--- app/src/main/java/com/example/examenes/LoginActivity.java
+++ app/src/main/java/com/example/examenes/LoginActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.examenes.helper.Menu;
@@ -18,9 +21,10 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUserName;
     private EditText etPassword;
     private CheckBox cbRememberUser;
-    private Button btnCreateUser;
+    private TextView btnCreateUser;
     private Button btnLogin;
-
+    private ImageButton ibChangePasswordVisibility;
+    private boolean showPassword = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +34,9 @@ public class LoginActivity extends AppCompatActivity {
         cbRememberUser = findViewById(R.id.cbRememberUser);
         btnCreateUser = findViewById(R.id.btnCreateUser);
         btnLogin = findViewById(R.id.btnLogin);
+        ibChangePasswordVisibility = findViewById(R.id.ibChangePasswordVisibility);
         Menu.Init(this, R.id.toolbarLogin);
+
         this.btnCreateUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,13 +47,27 @@ public class LoginActivity extends AppCompatActivity {
         this.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!isValid()){
+                /*if(!isValid()){
                     Toast.makeText(LoginActivity.this, "Completar datos",Toast.LENGTH_SHORT).show();
                     return;
-                }
+                }*/
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 intent.putExtra("USER_NAME", etUserName.getText().toString());
                 startActivity(intent);
+            }
+        });
+        ibChangePasswordVisibility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(showPassword){
+                    showPassword = false;
+                    etPassword.setInputType(InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+                    ibChangePasswordVisibility.setImageResource(R.drawable.ic_baseline_visibility_off_24);
+                }else{
+                    showPassword = true;
+                    etPassword.setInputType(InputType.TYPE_CLASS_TEXT);
+                    ibChangePasswordVisibility.setImageResource(R.drawable.ic_baseline_visibility_24);
+                }
             }
         });
     }
